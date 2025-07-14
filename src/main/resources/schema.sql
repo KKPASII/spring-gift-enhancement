d2782bc3610f98dbe851d081a02a803e35895860
KKPASII <kkongpasi1005@gmail.com>
2025-07-14 23:17:14 +0900

    feat: 상품 고도화 기본 코드 준비

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE product
+(
+    id        INT AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(15)   NOT NULL,
+    price     INT           NOT NULL CHECK (price >= 0),
+    image_url VARCHAR(1024) NOT NULL
+);
+
+CREATE TABLE member (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE wish (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INT NOT NULL DEFAULT 1 CHECK (quantity > 0)
+);
